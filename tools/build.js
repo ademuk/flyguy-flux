1ce8bcb3d0b23ae6adab1f39bbf74a4fd3a42ac2
Adem Gaygusuz <adem@ardweb.co.uk>
2016-01-06 21:17:08 +0000

    Remove starter kit copyright comments

--- tools/build.js
+++ tools/build.js
@@ -1,13 +1,4 @@
 /**
- * React Starter Kit (http://www.reactstarterkit.com/)
- *
- * Copyright © 2014-2015 Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
-/**
  * Compiles the project from source files into a distributable
  * format and copies it to the output (build) folder.
  */
